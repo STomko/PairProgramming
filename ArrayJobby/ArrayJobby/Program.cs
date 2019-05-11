46c07fb369c35502f4ba13119640742ce274c531
LukeC <42463260+BlazarQuazar@users.noreply.github.com>
2019-05-11 11:43:43 +0100

    Added removing of two lowest values + sum + new array generation

--- ArrayJobby/ArrayJobby/Program.cs
+++ ArrayJobby/ArrayJobby/Program.cs
@@ -12,7 +12,20 @@ namespace ArrayJobby
         {
             //Simple array of random numbers.
             int[] numArray = new int[] {85,12,3,72,111,26,18,55,90,8,-12,6};
+            
+            // Find two lowest numbers, remove from array, add together + append to end
+            int[] twoLowestValues = new int[2];            
 
+            for (int i = 0; i < 2; i++)
+			{
+                int lowestInt = numArray.Min();
+                numArray = numArray.Where(x => x != lowestInt).ToArray();
+                twoLowestValues[i]=lowestInt;
+			}
+
+            List<int> tempNumArray = numArray.ToList();
+            tempNumArray.Add(twoLowestValues.Sum());
+            numArray = tempNumArray.ToArray();
 
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
