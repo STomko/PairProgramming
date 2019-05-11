00949348a96bb1e0e8986dec476ad7554637adc0
STomko <stephen.tomkins3@gmail.com>
2019-05-11 11:17:32 +0100

    Added sumToArray Function
    Added the sumToArray function. This takes in two integers and an array.
    
    The two integers are added together and appended to the array.
    
    This is done by creating a new array one larger than the input array and appending the total to the end. The new array is then returned (with the intention of replacing the old array).
    
    Also added some debug code in the main function to test that it functions as intended and to test other functions.

--- ArrayJobby/ArrayJobby/Program.cs
+++ ArrayJobby/ArrayJobby/Program.cs
@@ -21,6 +21,37 @@ namespace ArrayJobby
 
             // What's this? a shameless plug?
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+
+            //DEBUG - This code is to test that the sumToArray function works.
+            numArray = sumToArray(5, 12, numArray);
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                Console.WriteLine(numArray[i]);
+            }
+            Console.ReadKey();
+            //DEBUG - End
+        }
+
+        //Takes in two integers, sums them and appends them to the array.
+        static int[] sumToArray(int num1, int num2, int[] numArray)
+        {
+            //Sums the two values.
+            int total = num1 + num2;
+
+            //Creates a new array one larger than the input array (to store the new total)
+            int[] returnArray = new int[numArray.Length + 1];
+
+            //Populates the new array with the existing array.
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                returnArray[i] = numArray[i];
+            }
+
+            //Adds the new total to the end of the new array
+            returnArray[returnArray.Length-1] = total;
+
+            return returnArray;
         }
     }
 }
